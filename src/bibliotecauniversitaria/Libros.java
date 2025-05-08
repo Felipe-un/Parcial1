f532e0829b098180754dc0788fbde0b45565a17c
Estudiante <Estudiante@10.220.0.152>
2025-05-08 15:43:13 -0500

    Segundo avance

---
+++ src/bibliotecauniversitaria/Libros.java
@@ -0,0 +1,17 @@
+package bibliotecauniversitaria;
+/**
+ *
+ * @author Estudiante
+ */
+public class Libros {
+    
+    String titulo;
+    boolean disponible = true;
+
+    Libros(String titulo) {
+        this.titulo = titulo;
+    }    
+    
+}
+    
+
